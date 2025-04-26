becc582fd859ca56f8a18264854448186ed41097
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-26 17:05:27 +0300

    Db Context sınıfı oluşturuldu

---
+++ Infrastructure/Persistence/RentACar.Persistence/Context/RentACarContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Context
+{
+    public class RentACarContext : DbContext
+    {
+        public RentACarContext()
+        {
+        }
+        public RentACarContext(DbContextOptions<RentACarContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarDescription> CarDescriptions { get; set; }
+        public DbSet<CarFeatures> CarFeatures { get; set; }
+        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Features> Features { get; set; }
+        public DbSet<FooterAddress> FooterAddresses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<TagCloud> TagClouds { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Infrastructure/Persistence/RentACar.Persistence/Context/RentACarContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Context
+{
+    public class RentACarContextFactory : IDesignTimeDbContextFactory<RentACarContext>
+    {
+        public RentACarContext CreateDbContext(string[] args)
+        {
+
+            var basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..\\RentACar.WebAPI"));
+            // appsettings.json dosyasını oku
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Connection string'i al
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // DbContextOptions oluştur
+            var optionsBuilder = new DbContextOptionsBuilder<RentACarContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new RentACarContext(optionsBuilder.Options);
+        }
+    }
+}
