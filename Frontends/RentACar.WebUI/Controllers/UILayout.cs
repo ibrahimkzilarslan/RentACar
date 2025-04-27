a1b740c919e2a1946f2c946d38979f7706daf7f3
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-27 22:48:15 +0300

    Controller'lar eklendi

---
+++ Frontends/RentACar.WebUI/Controllers/UILayout.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACar.WebUI.Controllers
+{
+    public class UILayout : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hakkımızda";
+            ViewBag.v2 = "Hakkımızda";
+            return View();
+        }
+    }
+}
