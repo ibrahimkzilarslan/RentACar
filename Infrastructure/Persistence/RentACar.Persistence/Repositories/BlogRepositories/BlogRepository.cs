85ff54d4aa078aa05e72062d90aa2c700919de42
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-27 14:19:57 +0300

    BlogRepositories oluşturuldu

---
+++ Infrastructure/Persistence/RentACar.Persistence/Repositories/BlogRepositories/BlogRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Application.Interfaces.BlogInterfaces;
+using RentACar.Domain.Entitites;
+using RentACar.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Repositories.BlogRepositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly RentACarContext _context;
+
+        public BlogRepository(RentACarContext context)
+        {
+            _context = context;
+        }
+
+        public List<Blog> GetAllBlogsWithAuthors()
+        {
+            var values = _context.Blogs.Include(x => x.Author).ToList();
+            return values;
+        }
+
+        public List<Blog> GetBlogByAuthorId(int id)
+        {
+            var values = _context.Blogs.Include(x => x.Author).Where(y => y.BlogID == id).ToList();
+            return values;
+        }
+
+        public List<Blog> GetLast3BlogsWithAuthors()
+        {
+            var values = _context.Blogs.Include(x => x.Author).OrderByDescending(x => x.BlogID).Take(3).ToList();
+            return values;
+        }
+    }
+}
