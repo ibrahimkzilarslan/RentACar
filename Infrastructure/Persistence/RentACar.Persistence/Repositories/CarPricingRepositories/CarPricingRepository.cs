d3e54725d85b85255d663ab51acc4da489ce8d06
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-27 14:20:15 +0300

    CarPricingRepositories oluşturuldu

---
+++ Infrastructure/Persistence/RentACar.Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Application.Interfaces.CarPricingInterfaces;
+using RentACar.Domain.Entitites;
+using RentACar.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Repositories.CarPricingRepositories
+{
+    public class CarPricingRepository : ICarPricingRepository
+    {
+        private readonly RentACarContext _context;
+
+        public CarPricingRepository(RentACarContext context)
+        {
+            _context = context;
+        }
+
+        public List<CarPricing> GetCarsPricingsWithCars()
+        {
+            var values = _context.CarPricings.Include(x => x.Car).ThenInclude(y => y.Brand).Include(x => x.Pricing).Where(z => z.PricingID == 2).ToList();
+            return values;
+        }
+    }
+}
