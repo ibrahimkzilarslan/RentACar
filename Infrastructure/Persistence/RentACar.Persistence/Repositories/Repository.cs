ec3788704489da9299e6b232485fe9da9c63ca02
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-27 14:21:29 +0300

    Repository sınıfı oluşturuldu

---
+++ Infrastructure/Persistence/RentACar.Persistence/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Application.Interfaces;
+using RentACar.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly RentACarContext _rentACarContext;
+
+        public Repository(RentACarContext rentACarContext)
+        {
+            _rentACarContext = rentACarContext;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            _rentACarContext.Set<T>().Add(entity);
+            await _rentACarContext.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _rentACarContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _rentACarContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _rentACarContext.Set<T>().Remove(entity);
+            await _rentACarContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _rentACarContext.Set<T>().Update(entity);
+            await _rentACarContext.SaveChangesAsync();
+        }
+    }
+}
