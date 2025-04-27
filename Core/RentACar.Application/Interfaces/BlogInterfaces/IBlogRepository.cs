511372a4fc8667ae082ac756c78a8ff255aa0018
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-27 14:22:37 +0300

    BlogInterfaces oluşturuldu

---
+++ Core/RentACar.Application/Interfaces/BlogInterfaces/IBlogRepository.cs
@@ -0,0 +1,19 @@
+﻿using RentACar.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Interfaces.BlogInterfaces
+{
+    public interface IBlogRepository
+    {
+        public List<Blog> GetLast3BlogsWithAuthors();
+
+        public List<Blog> GetAllBlogsWithAuthors();
+
+        public List<Blog> GetBlogByAuthorId(int id);
+    }
+}
