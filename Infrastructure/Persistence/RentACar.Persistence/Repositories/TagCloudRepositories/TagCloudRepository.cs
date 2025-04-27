02debb51c95da69f3b84afd497dea5d555be39e9
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-27 14:21:15 +0300

    TagCloudRepository oluşturuldu

---
+++ Infrastructure/Persistence/RentACar.Persistence/Repositories/TagCloudRepositories/TagCloudRepository.cs
@@ -0,0 +1,22 @@
+﻿using RentACar.Application.Interfaces.TagCloudInterfaces;
+using RentACar.Domain.Entitites;
+using RentACar.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Repositories.TagCloudRepositories
+{
+    public class TagCloudRepository : ITagCloudRepository
+    {
+
+        private readonly RentACarContext _context;
+        public List<TagCloud> GetTagCloudByBlogId(int id)
+        {
+            var values = _context.TagClouds.Where(x => x.BlogID == id).ToList();
+            return values;
+        }
+    }
+}
