07bf93d5f431b065c54371201621974159b5a1ce
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-27 14:19:33 +0300

    Program.cs de gerekli registirationlar girildi

--- Presentation/RentACar.WebAPI/Program.cs
+++ Presentation/RentACar.WebAPI/Program.cs
@@ -1,15 +1,102 @@
+using RentACar.Application.Features.RepositoryPattern;
+using RentACar.Application.Interfaces.BlogInterfaces;
+using RentACar.Application.Interfaces.CarInterfaces;
+using RentACar.Application.Interfaces.CarPricingInterfaces;
+using RentACar.Application.Interfaces.TagCloudInterfaces;
+using RentACar.Application.Interfaces;
+using RentACar.Persistence.Context;
+using RentACar.Persistence.Repositories.BlogRepositories;
+using RentACar.Persistence.Repositories.CarPricingRepositories;
+using RentACar.Persistence.Repositories.CarRepositories;
+using RentACar.Persistence.Repositories.CommentRepositories;
+using RentACar.Persistence.Repositories.TagCloudRepositories;
+using RentACar.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using RentACar.Application.Features.CQRS.Handlers.AboutHandlers;
+using RentACar.Application.Features.CQRS.Handlers.BannerHandlers;
+using RentACar.Application.Features.CQRS.Handlers.BrandHandlers;
+using RentACar.Application.Features.CQRS.Handlers.CarHandlers;
+using RentACar.Application.Features.CQRS.Handlers.CategoryHandlers;
+using RentACar.Application.Features.CQRS.Handlers.ContactHandlers;
+using RentACar.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Context ve Repository Sýnýflarý
+builder.Services.AddScoped<RentACarContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(ICarRepository), typeof(CarRepository));
+builder.Services.AddScoped(typeof(IBlogRepository), typeof(BlogRepository));
+builder.Services.AddScoped(typeof(ICarPricingRepository), typeof(CarPricingRepository));
+builder.Services.AddScoped(typeof(ITagCloudRepository), typeof(TagCloudRepository));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(CommentRepository<>));
+
+
+
+
+// About Sýnýfý Konfigürasyonu
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+// Banner Sýnýfý Konfigürasyonu
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+
+
+// Brand Sýnýfý Konfigürasyonu
+builder.Services.AddScoped<UpdateBrandCommandHandler>();
+builder.Services.AddScoped<RemoveBrandCommandHandler>();
+builder.Services.AddScoped<CreateBrandCommandHandler>();
+builder.Services.AddScoped<GetBrandQueryHandler>();
+builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+
+// Car Sýnýfý Konfigürasyonu
+builder.Services.AddScoped<UpdateCarCommandHandler>();
+builder.Services.AddScoped<RemoveCarCommandHandler>();
+builder.Services.AddScoped<CreateCarCommandHandler>();
+builder.Services.AddScoped<GetCarQueryHandler>();
+builder.Services.AddScoped<GetCarByIdQueryHandler>();
+builder.Services.AddScoped<GetCarWithBrandQueryHandler>();
+builder.Services.AddScoped<GetLast5CarsWithBrandQueryHandler>();
+
+
+// Category Sýnýfý Konfigürasyonu
+builder.Services.AddScoped<UpdateCategoryCommandHandler>();
+builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+builder.Services.AddScoped<CreateCategoryCommandHandler>();
+builder.Services.AddScoped<GetCategoryQueryHandler>();
+builder.Services.AddScoped<GetCategoryByIdQueryHandler>();
+
+
+// Contact Sýnýfý Konfigürasyonu
+builder.Services.AddScoped<UpdateContactCommandHandler>();
+builder.Services.AddScoped<RemoveContactCommandHandler>();
+builder.Services.AddScoped<CreateContactCommandHandler>();
+builder.Services.AddScoped<GetContactQueryHandler>();
+builder.Services.AddScoped<GetContactByIdQueryHandler>();
+
+// Mediator registiration
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddDbContext<RentACarContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
