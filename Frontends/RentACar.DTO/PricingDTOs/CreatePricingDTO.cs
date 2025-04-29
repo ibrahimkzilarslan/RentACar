bdc5ee0a8b5ef2db3d96b6175b02f13e9b42bd70
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-29 19:36:20 +0300

    PricingDTOs eklendi

---
+++ Frontends/RentACar.DTO/PricingDTOs/CreatePricingDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.DTO.PricingDTOs
+{
+    public class CreatePricingDTO
+    {
+      
+        public string Name { get; set; }
+    }
+}
