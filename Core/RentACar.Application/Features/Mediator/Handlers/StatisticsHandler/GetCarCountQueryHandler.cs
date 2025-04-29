f7c1422ef438da410cb4403792633c90d3495ffd
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-29 23:48:59 +0300

    GetCarCountQueryHandler eklendi

---
+++ Core/RentACar.Application/Features/Mediator/Handlers/StatisticsHandler/GetCarCountQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using RentACar.Application.Features.Mediator.Queries.StatisticsQueries;
+using RentACar.Application.Features.Mediator.Results.FooterAddressResults;
+using RentACar.Application.Features.Mediator.Results.StatisticsResults;
+using RentACar.Application.Interfaces;
+using RentACar.Application.Interfaces.CarInterfaces;
+using RentACar.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Features.Mediator.Handlers.StatisticsHandler
+{
+    public class GetCarCountQueryHandler : IRequestHandler<GetCarCountQuery, GetCarCountQueryResults>
+    {
+
+        private readonly ICarRepository _repository;
+
+        public GetCarCountQueryHandler(ICarRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCarCountQueryResults> Handle(GetCarCountQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetCarCount();
+            return new GetCarCountQueryResults
+            {
+                CarCount = value,
+
+            };
+        }
+    }
+}
