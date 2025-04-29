5ec028a2e8d6c6792bd8f163cb83e8efae32fe1c
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-29 19:35:51 +0300

    FooterAddressDTOs eklendi

---
+++ Frontends/RentACar.DTO/FooterAddressDTOs/CreateFooterAddressDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.DTO.FooterAddressDTOs
+{
+    public class CreateFooterAddressDTO
+    {
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+    }
+}
