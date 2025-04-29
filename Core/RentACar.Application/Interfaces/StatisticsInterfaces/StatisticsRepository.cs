4a04c3c64c2c986a3c3ba0f2588160aa3c04b29d
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-29 23:47:23 +0300

    StatisticsRepository eklendi

---
+++ Core/RentACar.Application/Interfaces/StatisticsInterfaces/StatisticsRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Interfaces.StatisticsInterfaces
+{
+    public interface StatisticsRepository
+    {
+        int GetLocationCount();
+        int GetAuthorCount();
+        int GetBlogCount();
+        int GetBrandCount();
+        double GetAvgRentPriceForDaily();
+        double GetAvgRentPriceForWeekly();
+        double GetAvgRentPriceForMountly();
+        int GetCarCountByTransmissionIsAuto();
+        string BrandNameByMaxCar();
+        string BlogTitleByMaxBlogComment();
+        int GetCarCountByKmSmallerThen1000();
+        int GetCarCountByFuelGasolineOrDiesel();
+        int GetCarCountByFuelElectric();
+        string GetCarBrandAndModelByRentPriceDailyMax();
+        string GetCarBrandAndModelByRentPriceDailyMin();
+
+    }
+}
