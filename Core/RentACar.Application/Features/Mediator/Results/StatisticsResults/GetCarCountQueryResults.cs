32b1e9ebd4da5bb1bfe0d428c4ce36d338625694
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-29 23:48:13 +0300

    GetCarCountQueryResults eklendi

---
+++ Core/RentACar.Application/Features/Mediator/Results/StatisticsResults/GetCarCountQueryResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Features.Mediator.Results.StatisticsResults
+{
+    public class GetCarCountQueryResults
+    {
+        public int CarCount { get; set; }
+    }
+}
