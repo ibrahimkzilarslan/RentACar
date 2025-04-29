7359508bdd6b595fd409b233209606f264038f67
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-29 23:46:27 +0300

    StatisticsController eklendi

---
+++ Presentation/RentACar.WebAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Features.Mediator.Queries.StatisticsQueries;
+
+namespace RentACar.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StatisticsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("GetCarCount")]
+        public async Task<IActionResult> GetCarCount()
+        {
+            var values = await _mediator.Send(new GetCarCountQuery());
+            return Ok(values);
+        }
+    }
+}
