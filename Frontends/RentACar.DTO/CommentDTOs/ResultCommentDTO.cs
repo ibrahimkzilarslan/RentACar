df3883ae22b73d95f6a5b795a011ce02539bd08d
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-29 19:34:45 +0300

    CommentDTOs eklendi

---
+++ Frontends/RentACar.DTO/CommentDTOs/ResultCommentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.DTO.CommentDTOs
+{
+    public class ResultCommentDTO
+    {
+        public int CommentID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int BlogID { get; set; } 
+    }
+}
