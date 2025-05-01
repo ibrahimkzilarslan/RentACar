5e5934c8a438a681b687f8130c1f2c6895b738b0
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-05-01 21:09:23 +0300

    kalan StatisticsQueries'ler eklendi

---
+++ Core/RentACar.Application/Features/Mediator/Queries/StatisticsQueries/GetCarBrandAndModelByRentPriceDailyMinQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using RentACar.Application.Features.Mediator.Results.StatisticsResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Features.Mediator.Queries.StatisticsQueries
+{
+    public class GetCarBrandAndModelByRentPriceDailyMinQuery : IRequest<GetCarBrandAndModelByRentPriceDailyMinQueryResults>
+    {
+    }
+}
