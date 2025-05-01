20f54f1be2305b7ef416c3ae655f4be16c7aba54
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-05-01 21:09:00 +0300

    kalan StatisticsResult'lar eklendi

---
+++ Core/RentACar.Application/Features/Mediator/Results/StatisticsResults/GetCarCountByFuelElectricQueryResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Features.Mediator.Results.StatisticsResults
+{
+    public class GetCarCountByFuelElectricQueryResults
+    {
+        public int CarCountByFuelElectric { get; set; }
+    }
+}
---
+++ Core/RentACar.Application/Features/Mediator/Results/StatisticsResults/GetCarCountByKmSmallerThen1000QueryResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Features.Mediator.Results.StatisticsResults
+{
+    public class GetCarCountByKmSmallerThen1000QueryResults
+    {
+        public int CarCountByKmSmallerThen1000 { get; set; }
+    }
+}
