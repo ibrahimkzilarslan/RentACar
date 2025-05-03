1b278dea34137868fdef19e2abb990dda815e693
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-05-03 17:18:00 +0300

    ReservationController eklendi

---
+++ Presentation/RentACar.WebAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Features.Mediator.Commands.ReservationCommands;
+using RentACar.Application.Features.Mediator.Commands.TestimonialCommands;
+
+namespace RentACar.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IMediator _mediator; 
+
+        public ReservationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateReservation(CreateReservationCommands command)
+        {
+            await _mediator.Send(command);
+            return Ok("Rezervasyon eklendi");
+        }
+
+    }
+}
