aaae94969b26d052184e164e9eda091745b28225
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-05-03 17:12:55 +0300

    Add project files.

---
+++ Core/RentACar.Domain/Entities/Features.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Domain.Entities
+{
+    public class Features
+    {
+        public int FeaturesID { get; set; }
+        public string Name { get; set; }
+        public List<CarFeatures> CarFeatures { get; set; }
+    }
+}
---
+++ Core/RentACar.Domain/Entities/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Domain.Entities
+{
+    public class Location
+    {
+        public int LocationID { get; set; }
+        public string Name { get; set; }
+        public List<RentCar> RentCars { get; set; }
+        public List<Reservation> PickUpReservation { get; set; }
+        public List<Reservation> DropOffReservation { get; set; }
+
+
+    }
+}
