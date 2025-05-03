95a7fac13be5ae4a01d83a50916aeed91b3c54ca
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-05-03 17:18:46 +0300

    RentACarController eklendi

---
+++ Presentation/RentACar.WebAPI/Controllers/RentACarsController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Features.Mediator.Queries.RentCarQueries;
+
+namespace RentACar.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentACarsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RentACarsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRentCarListByLocation(int locationID,bool available )
+        {
+            GetRentCarQuery getRentCarQuery = new GetRentCarQuery
+            {
+                LocationID = locationID,
+                Available = available
+            };
+            var values = await _mediator.Send(getRentCarQuery);
+            return Ok(values);
+        }
+    }
+}
