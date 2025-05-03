595bcecdcb5807e2f20c52b1657fb759b4ab5fe7
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-05-03 17:20:55 +0300

    RentCarDTOs eklendi

---
+++ Frontends/RentACar.DTO/RentCarDTOs/FilterRentCarDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.DTO.RentCarDTOs
+{
+    public class FilterRentCarDTO
+    {
+        public int carID { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string CoverImageUrl { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
